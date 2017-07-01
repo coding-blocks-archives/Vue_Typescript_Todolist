14b0d0013a057c942f374216cbd9da58164e6634
Arnav Gupta <arnav@codingblocks.com>
2017-07-02 02:03:31 +0530

    basic todolist with vue
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ src/components/TodoList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <input @keydown.enter="addTodo(newtask)" type="text" v-model="newtask"/>
+    <button @click="addTodo(newtask)">Add</button>
+    <button @click="todos.reverse()">Flip</button>
+    <ul id="todo-list">
+      <li v-for="todo in todos" :key="todo.id">
+        <input @change="todo.done = true" type="checkbox" :checked="todo.done"/>
+        <span>{{todo.task}}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        todos: [
+          {id: 1, task: 'First task', done: true},
+          {id: 2, task: 'Second task', done: false}
+        ]
+      }
+    },
+    methods: {
+      addTodo (newtodo) {
+        this.todos.push({
+          id: this.todos.length + 1,
+          task: newtodo,
+          done: false
+        })
+      }
+    }
+  }
+</script>
+
+<style>
+#todo-list {
+  width: 300px;
+  margin-left: auto;
+  margin-right: auto;
+  text-align: left;
+}
+</style>
